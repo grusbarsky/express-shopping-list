19c09f7ccae9a350573b8d59715059d1265f48b9
grusbarsky <gretarusbarsky@gmail.com>
2021-08-24 10:34:27 -0500

    first commit

---
+++ routes/items.js
@@ -0,0 +1,60 @@
+const Item = require('../item');
+const express = require('express');
+
+const router = express.Router();
+
+/** get item */
+
+router.get('', (req, res, next) => {
+  try {
+    return res.json({ items: Item.findAll() });
+  } catch(err){
+    return next(err)
+  }
+});
+
+/** post new item */
+
+router.post('', (req, res, next) => {
+  try {
+    let newItem = new Item(req.body.name, req.body.price);
+    return res.json({item: newItem});
+  } catch (err) {
+    return next(err)
+  }
+});
+
+/** get name of item */
+
+router.get('/:name', (req, res, next) => {
+  try {
+    let foundItem = Item.find(req.params.name);
+    return res.json({item:foundItem});
+  } catch(err){
+    return next(err)
+  }
+});
+
+/** update item */
+
+router.patch('/:name', (req, res, next) => {
+  try {
+    let foundItem = Item.update(req.params.name, req.body);
+    return res.json({ item: foundItem });
+  } catch (err) {
+    return next(err)
+  }
+});
+
+/** delete item */
+
+router.delete('/:name', (req, res, next) => {
+  try {
+    Item.remove(req.params.name);
+    return res.json({message:'Deleted'});
+  } catch (err) {
+    return next(err)
+  }
+});
+
+module.exports = router;
